7a99bc7eb041bad380f8ff6b376add99e0a5a6f0
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-12-02 20:23:31 +0200

    feat: create solutions for all date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -12,15 +12,15 @@
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example:
  *    'December 17, 1995 03:24:00'    => Date()
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -28,14 +28,14 @@ function parseDataFromRfc2822(/* value */) {
  * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example :
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -44,7 +44,7 @@ function parseDataFromIso8601(/* value */) {
  * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
  *
  * @param {date} date
- * @return {bool}
+ * @return {boolean}
  *
  * @example :
  *    Date(1900,1,1)    => false
@@ -53,8 +53,8 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(date.getFullYear(), 1, 29).getDate() === 29;
 }
 
 
@@ -73,10 +73,16 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const diff = endDate - startDate;
+  const hours = Math.trunc(diff / (1000 * 60 * 60));
+  const minutes = Math.trunc((diff - hours * 1000 * 60 * 60) / (1000 * 60));
+  const seconds = Math.trunc((diff - hours * 1000 * 60 * 60 - minutes * 1000 * 60) / 1000);
+  const milisecs = diff - hours * 1000 * 60 * 60 - minutes * 1000 * 60 - seconds * 1000;
+  return `${hours > 9 ? hours : `0${hours}`}:${
+    minutes > 9 ? minutes : `0${minutes}`
+  }:${seconds > 9 ? seconds : `0${seconds}`}.${milisecs || '000'}`;
 }
-
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,8 +100,21 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  let hours = date.getUTCHours();
+  hours = hours > 12 ? (hours - 12) : (hours);
+  const minutes = date.getMinutes();
+  const hoursAngle = (360 / 12) * hours + minutes * (360 / 12 / 60);
+  const minutesAngle = (360 / 60) * minutes;
+  if (minutes === 0 && hours === 0) {
+    return 0;
+  }
+  const max = Math.max(hoursAngle, minutesAngle);
+  const min = Math.min(hoursAngle, minutesAngle);
+  if (max - min < 180) {
+    return (max - min) / (180 / Math.PI);
+  }
+  return (360 - (max - min)) / (180 / Math.PI);
 }
 
 
