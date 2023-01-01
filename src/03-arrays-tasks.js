a94ff5a9073ffa2d6ce533a1c79decd18e822624
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-01-01 16:14:34 +0200

    feat: solved all except last in clases

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -556,8 +556,17 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const unique = Array.from(new Set(array.map((el) => keySelector(el))));
+  return unique.reduce((acc, el) => {
+    acc.push([
+      el,
+      array
+        .filter((elem) => keySelector(elem) === el)
+        .map((element) => valueSelector(element)),
+    ]);
+    return acc;
+  }, []);
 }
 
 
@@ -574,8 +583,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((acc, el) => acc.concat(childrenSelector(el)), []);
 }
 
 
@@ -591,8 +600,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, elem) => acc[elem], arr);
 }
 
 
@@ -614,8 +623,12 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const head = arr.slice(0, arr.length / 2);
+  const tail = arr.slice(Math.round(arr.length / 2), arr.length);
+  return tail
+    .concat(arr.length % 2 ? [arr[Math.round(arr.length / 2 - 1)]] : [])
+    .concat(head);
 }
 
 
--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -106,8 +106,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let currentAttempt = 0;
+  return function f() {
+    try {
+      func();
+    } catch (e) {
+      if (currentAttempt < attempts) {
+        currentAttempt += 1;
+        f();
+      }
+    }
+    return func();
+  };
 }
 
 
@@ -134,8 +145,13 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1).slice(0, -1)}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1).slice(0, -1)}) ends`);
+    return result;
+  };
 }
 
 
@@ -152,8 +168,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => fn(...args1, ...args);
 }
 
 
